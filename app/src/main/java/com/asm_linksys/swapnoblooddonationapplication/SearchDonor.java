cb7fe4b8a538098e649aa957a67425fa52ed4ccc
Md. Asiful Haque <kh3laghar@gmail.com>
2016-09-29 12:10:51 +0600

    Abstract Model

---
+++ app/src/main/java/com/asm_linksys/swapnoblooddonationapplication/SearchDonor.java
@@ -0,0 +1,61 @@
+package com.asm_linksys.swapnoblooddonationapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import com.asm_linksys.swapnoblooddonationApplication.R;
+
+
+//Implement OnItemSelectedListener So that it could Reply on Spinner Selection
+public class SearchDonor extends Activity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstancestate) {
+        super.onCreate(savedInstancestate);
+        //Setting Content view to activity_search_donor
+        setContentView(R.layout.activity_search_donor);
+        //Selecting the Array of Blood Group List
+    }
+
+    /**
+     * <p>Callback method to be invoked when an item in this view has been
+     * selected. This callback is invoked only when the newly selected
+     * position is different from the previously selected position or if
+     * there was no selected item.</p>
+     * <p>
+     * Impelmenters can call getItemAtPosition(position) if they need to access the
+     * data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the selection happened
+     * @param view     The view within the AdapterView that was clicked
+     * @param position The position of the view in the adapter
+     * @param id       The row id of the item that is selected
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    /**
+     * Callback method to be invoked when the selection disappears from this
+     * view. The selection can disappear for instance when touch is activated
+     * or when the adapter becomes empty.
+     *
+     * @param parent The AdapterView that now contains no selected item.
+     */
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
+// Auto generate Part End Here
