5a3e3d442673f493e8363920a9d2f6305349bc76
Md. Asiful Haque <kh3laghar@gmail.com>
2016-10-13 03:03:41 +0600

    Changed to abstract Class

---
+++ app/src/main/java/com/asm_linksys/swapnoblooddonationapplication/RegularMethods.java
@@ -0,0 +1,17 @@
+package com.asm_linksys.swapnoblooddonationapplication;
+
+/**
+ * Created by Md.Asiful on 13-Oct-16.
+ */
+
+public abstract class RegularMethods {
+    public abstract void addUser();
+
+    public abstract void searchUser();
+
+    public abstract void removeUser();
+
+    public abstract void updateUser();
+
+
+}
